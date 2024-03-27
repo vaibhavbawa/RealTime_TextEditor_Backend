47a7d298b2ba829f4f56e727004b4b7507370b80
vaibhavbawamq@gmail.com <vaibhavbawamq@gmail.com>
2024-03-27 11:34:46 +0530

    basic changes

--- socket.io/index.js
+++ socket.io/index.js
@@ -6,7 +6,6 @@ const server =  createServer(app);
 
 
 
-const PORT = 3000;
 
 const io = new Server(server,{
     cors:{
@@ -18,15 +17,17 @@ const io = new Server(server,{
 
 io.on("connection" ,(socket)=>{
 console.log("user connected ",socket.id);
-io.emit("msg","welcome to socket io server");
-io.emit("msg","welcome to socket io server");
+socket.emit("msg","welcome to socket io server");
+// io.emit("msg","welcome to socket io server");
+// socket.broadcast.emit("msg",`${socket.id},joined the server`)
 socket.on("rc",(data)=>{
     console.log("RC data ", data);
-    
-    })
+})
 socket.on("disconnect",()=>{
-console.log("user disconnected ");
+console.log("user disconnected ",socket.id);
+})
+})
 
-})
-})
+
+const PORT = 3000;
 server.listen(PORT,()=> console.log(`Listen on port ${PORT}`));
